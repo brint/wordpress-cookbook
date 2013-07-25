0266cd1035c5ab7bb8f7ac126e2919aed91d8205
Veered <lucashansne@gmail.com>
2013-07-25 16:33:37 -0700

    Added support for the ACS WordPress plugin.

---
+++ attributes/plugin_acs.rb
@@ -0,0 +1,6 @@
+default['wordpress']['plugin']['acs']['name'] = 'acs-plugin-for-wordpress'
+default['wordpress']['plugin']['acs']['version'] = 'dev'
+
+default['wordpress']['plugin']['acs']['namespace'] = ''
+default['wordpress']['plugin']['acs']['realm'] = ''
+default['wordpress']['plugin']['acs']['key'] = ''
---
+++ recipes/plugin_acs.rb
@@ -0,0 +1,47 @@
+#
+# Author:: Lucas Hansen <lucash@opscode.com>
+# Cookbook Name:: wordpress
+# Recipe:: plugin_acs
+#
+# Copyright:: 2013, Opscode, Inc <legal@opscode.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/mixin/shell_out'
+::Chef::Resource::Template.send :include, Chef::Mixin::ShellOut
+
+config = node['wordpress']['plugin']['acs']
+
+wordpress_plugin 'acs' do
+  action :install
+  plugin_name config['name']
+  version config['version']
+end
+
+template "acs_config" do
+  action :create
+  source 'plugin_acs_config.erb'
+  path(lazy do
+    base_dir = node['wordpress']['dir'].gsub('/', '\\')
+    cmd = shell_out!("#{node['wordpress']['bin']} --path=\"#{base_dir}\" plugin path #{config['name']}")
+    dir = File.dirname(cmd.stdout.gsub('\\', '/'))
+    "#{dir}/acs-wp-plugin-config.php"
+  end)
+  variables config
+end
+
+wordpress_plugin 'acs' do
+  action :activate
+  plugin_name config['name']
+end
