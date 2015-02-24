1905e411a4d18c1be0af32d012ab7f3a689f5dab
Brint O'Hearn <brint.ohearn@rackspace.com>
2015-02-24 09:36:05 -0600

    Updates to get PHP-FPM/Nginx working out of the box with CentOS

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -22,7 +22,7 @@ node.set_unless['php-fpm']['pools'] = []
 include_recipe "php-fpm"
 
 php_fpm_pool "wordpress" do
-  listen "127.0.0.1:9000"
+  listen "127.0.0.1:9001"
   user node['wordpress']['install']['user']
   group node['wordpress']['install']['group']
   if node['platform'] == 'ubuntu' and node['platform_version'] == '10.04'
@@ -36,7 +36,7 @@ end
 
 include_recipe "php::module_mysql"
 
-node.set['nginx']['default_site_enabled'] = false
+node.set_unless['nginx']['default_site_enabled'] = false
 include_recipe "nginx"
 
 include_recipe "wordpress::app"
@@ -50,4 +50,14 @@ template "#{node['nginx']['dir']}/sites-enabled/wordpress.conf" do
     :server_port      => node['wordpress']['server_port']
   )
   action :create
+end
+
+# The following block is specifically for OS's like CentOS that include a
+# default site as a part of the install. This block will only be triggered if
+# node['nginx']['default_site_enable'] is set to false.
+if node['platform_family'] == 'rhel' && !node['nginx']['default_site_enabled']
+  file File.join(node['nginx']['dir'], 'conf.d', 'default.conf') do
+    action :delete
+    notifies :reload, 'service[nginx]'
+  end
 end
