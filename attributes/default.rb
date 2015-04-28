60e04ae1f547da95728c37ac2c74843b1e60dab7
Peter Green <peter.green@aztek-native.com>
2015-04-28 10:58:44 +0100

    Create attribute to hold plugin list.

--- attributes/default.rb
+++ attributes/default.rb
@@ -101,3 +101,6 @@ else
 end
 
 default['wordpress']['php_options'] = { 'php_admin_value[upload_max_filesize]' => '50M', 'php_admin_value[post_max_size]' => '55M' }
+
+default['wordpress']['plugins'] = {}
+
