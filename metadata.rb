7761a7510c8d9afd9be7bf57d83dfd4bd8a858bc
Alejandro Diaz <rubenalejandrodiaz@gmail.com>
2016-06-23 12:00:19 -0700

    Updating mysql2_chef_gem

--- metadata.rb
+++ metadata.rb
@@ -16,7 +16,7 @@ end
 depends "apache2", ">= 2.0.0"
 depends "database", ">= 1.6.0"
 depends "mysql", ">= 6.0"
-depends "mysql2_chef_gem", "~> 1.0.1"
+depends "mysql2_chef_gem", ">= 1.1.0"
 depends "build-essential"
 depends "iis", ">= 1.6.2"
 depends "tar", ">= 0.3.1"
